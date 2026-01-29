35da8100242e4ce28ffca540ec1bc0f52b1a2287
Qingzhao Yan <yanyifei66@gmail.com>
2026-01-29 12:41:07 +0800

    move lut to luts, functional CLI

---
+++ cli.py
@@ -0,0 +1,164 @@
+import typer, pathlib, asyncio, os, shutil
+from rich.progress import Progress, MofNCompleteColumn, BarColumn, TimeRemainingColumn, TextColumn
+from main import ThermalReportGenerator
+from typing import Literal, Annotated, Optional
+
+app = typer.Typer(help="A tool to generate report of DJI R-JPEG (Thermal Image) based on [b i]dji_irp[/b i]")
+
+@app.command(help="[b]FAST[/b] will try to locate your [b i]dji_irp[/b i] in $PATH or working dir. Will raise error if cannot find it.")
+def fast(
+    input_dir: Annotated[
+        Optional[pathlib.Path], typer.Argument(help="Directory of your input files (You can also pass multiple --input/-i to input files)")
+    ] = None,
+    input_files: Annotated[
+        Optional[list[pathlib.Path]], typer.Option("--input", "-i", help="Your input files (Alternative)")
+    ] = None,
+    output_dir: Annotated[
+        pathlib.Path, typer.Option("--output", "-o", help="Directory for saving PDFs")
+    ] = pathlib.Path('./reports'),
+    temp_dir: Annotated[
+        pathlib.Path, typer.Option("--temp", "-t", help="Directory for temporary RAW files")
+    ] = pathlib.Path('./temps'),
+    distance: Annotated[
+        float, typer.Option("--distance", "-dis", min=1.0, max=25.0)
+    ] = 5.0,
+    humidity: Annotated[
+        float, typer.Option("--humidity", "-hum", min=20.0, max=100.0)
+    ] = 50.0,
+    emissivity: Annotated[
+        float, typer.Option("--emissivity", "-emi", min=0.10, max=1.00)
+    ] = 0.95,
+    ambient: Annotated[
+        float, typer.Option("--ambient", "-amb", min=-40.0, max=80.0)
+    ] = 25.0,
+    reflection: Annotated[
+        float, typer.Option("--reflection", "-ref", min=-40.0, max=500.0)
+    ] = 25.0,
+    brightness: Annotated[
+        int, typer.Option("--brightness", "-bri", min=0, max=100)
+    ] = 50,
+    palette: Annotated[
+        Literal['white_hot', 'fulgurite', 'iron_red', 
+                'hot_iron', 'medical', 'arctic', 'rainbow1', 
+                'rainbow2', 'tint', 'black_hot'], 
+        typer.Option("--palette", "-p")
+    ] = 'iron_red',
+    colorbar_width: Annotated[
+        int, typer.Option("--cbwidth", "-cbw", min=1, max=100, help='Width of Temperature-Color Bar')
+    ] = 10,
+    cbborder: Annotated[
+        bool, typer.Option(help='Whether to show border of Temperature-Color Bar')
+    ] = False,
+    max_workers: Annotated[
+        int, typer.Option("--workers", "-ws", min=1, max=32, help='Max workers of concurrent process')
+    ] = 4
+):
+    main(
+        cli_path=shutil.which("dji_irp"),
+        input_dir=input_dir,
+        input_files=input_files,
+        output_dir=output_dir,
+        temp_dir=temp_dir,
+        distance=distance,
+        humidity=humidity,
+        emissivity=emissivity,
+        ambient=ambient,
+        reflection=reflection,
+        brightness=brightness,
+        palette=palette,
+        colorbar_width=colorbar_width,
+        cbborder=cbborder,
+        max_workers=max_workers
+    )
+
+@app.command(help="Require user to pass absolute path to [b i]dji_irp[/b i] executable")
+def main(
+    cli_path: Annotated[
+        pathlib.Path, typer.Argument(help='Absolute path to your complied [b i]dji_irp[/b i] executable')
+    ],
+    input_dir: Annotated[
+        Optional[pathlib.Path], typer.Argument(help="Directory of your input files (You can also pass multiple --input/-i to input files)")
+    ] = None,
+    input_files: Annotated[
+        list[pathlib.Path], typer.Option("--input", "-i", help="Your input files (Alternative)")
+    ] = [],
+    output_dir: Annotated[
+        pathlib.Path, typer.Option("--output", "-o", help="Directory for saving PDFs")
+    ] = pathlib.Path('./reports'),
+    temp_dir: Annotated[
+        pathlib.Path, typer.Option("--temp", "-t", help="Directory for temporary RAW files")
+    ] = pathlib.Path('./temps'),
+    distance: Annotated[
+        float, typer.Option("--distance", "-dis", min=1.0, max=25.0)
+    ] = 5.0,
+    humidity: Annotated[
+        float, typer.Option("--humidity", "-hum", min=20.0, max=100.0)
+    ] = 50.0,
+    emissivity: Annotated[
+        float, typer.Option("--emissivity", "-emi", min=0.10, max=1.00)
+    ] = 0.95,
+    ambient: Annotated[
+        float, typer.Option("--ambient", "-amb", min=-40.0, max=80.0)
+    ] = 25.0,
+    reflection: Annotated[
+        float, typer.Option("--reflection", "-ref", min=-40.0, max=500.0)
+    ] = 25.0,
+    brightness: Annotated[
+        int, typer.Option("--brightness", "-bri", min=0, max=100)
+    ] = 50,
+    palette: Annotated[
+        Literal['white_hot', 'fulgurite', 'iron_red', 
+                'hot_iron', 'medical', 'arctic', 'rainbow1', 
+                'rainbow2', 'tint', 'black_hot'], 
+        typer.Option("--palette", "-p")
+    ] = 'iron_red',
+    colorbar_width: Annotated[
+        int, typer.Option("--cbwidth", "-cbw", min=1, max=100, help='Width of Temperature-Color Bar')
+    ] = 10,
+    cbborder: Annotated[
+        bool, typer.Option(help='Whether to show border of Temperature-Color Bar')
+    ] = False,
+    max_workers: Annotated[
+        int, typer.Option("--workers", "-ws", min=1, max=32, help='Max workers of concurrent process')
+    ] = 4
+):
+    if not cli_path or not pathlib.Path(cli_path).exists():
+        raise FileNotFoundError("Cannot find dji_irp executable")
+    if not input_dir and not input_files:
+        raise ValueError("No any input")
+    if os.name == 'nt':
+        asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
+
+    async def __internal_async():
+        gen = ThermalReportGenerator(
+            input_dir=input_dir,
+            output_dir=output_dir,
+            temp_dir=temp_dir,
+            cli_path=cli_path,
+            distance=distance,
+            humidity=humidity,
+            emissivity=emissivity,
+            ambient=ambient,
+            reflection=reflection,
+            brightness=brightness,
+            palette=palette,
+            colorbar_width=colorbar_width,
+            colorbar_border=cbborder,
+            max_workers=max_workers
+        )
+        with Progress(
+            TextColumn("[progress.description]{task.description}"), BarColumn(), MofNCompleteColumn(), TimeRemainingColumn(),
+            transient=True
+        ) as progress:
+            dummy_task = progress.add_task('Please wait', total=None)
+            task = None
+            async for i, r in gen.run(input_files if input_files else None):
+                if not task:
+                    progress.remove_task(dummy_task)
+                    task = progress.add_task('Processing', total=i)
+                progress.advance(task, 1)
+    
+    asyncio.run(__internal_async())
+
+if __name__ == "__main__":
+    app()
--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-import os, datetime, exifread, uuid, asyncio, pathlib, json, traceback, fitz # PyMuPDF
+import os, datetime, pyexiv2, uuid, asyncio, pathlib, json, traceback, fitz # PyMuPDF
 from PIL import Image
 from weasyprint import HTML
 from jinja2 import Template
 from enum import Enum
 from concurrent.futures import ProcessPoolExecutor
+from typing import AsyncGenerator, Optional, Literal
 
 # 配置路径
-CLI_PATH = r"D:\Test\dji_thermal_sdk_v1.8_20250829\utility\bin\windows\release_x64\dji_irp.exe"  # 编译好的 dji_irp 路径
-INPUT_DIR = "./input_images"
-OUTPUT_DIR = "./reports"
-TEMP_DIR = "./temp"
-LUT_DIR = "./lut"
+LUT_DIR = pathlib.Path(__file__).parent / "luts"
 
 class ThermalPalette(Enum):
     white_hot = 0
@@ -31,7 +28,7 @@ def get_palette(palette: ThermalPalette):
     with open(palette_json, mode='r', encoding='utf-8') as f:
         return ', '.join(f"rgb({r},{g},{b})" for r, g, b in json.load(f).__reversed__())
 
-def convert_to_decimal(coords: tuple[int, int, float] | str) -> float:
+def convert_to_decimal(coords: tuple[float, float, float] | str) -> float:
     # coords 格式为 [23, 21, 28.4713]
     try:
         if isinstance(coords, str):
@@ -45,18 +42,25 @@ def convert_to_decimal(coords: tuple[int, int, float] | str) -> float:
 
 class ThermalReportGenerator:
     def __init__(self, 
+            input_dir: str | pathlib.Path,
+            output_dir: str | pathlib.Path,
+            temp_dir: str | pathlib.Path,
+            cli_path: str | pathlib.Path,
             distance: float = 5.0,
             humidity: float = 50.0,
             emissivity: float = 0.95,
             ambient: float = 25.0,
             reflection: float = 25.0,
             brightness: int = 50,
-            palette: ThermalPalette = ThermalPalette.iron_red,
+            palette: ThermalPalette | Literal['white_hot', 'fulgurite', 'iron_red',
+    'hot_iron', 'medical', 'arctic', 'rainbow1', 'rainbow2', 'tint', 'black_hot'] = ThermalPalette.iron_red,
+            colorbar_width: int = 10,
+            colorbar_border: bool = False,
             max_workers: int = 4
         ):
-        pathlib.Path(OUTPUT_DIR).mkdir(exist_ok=True)
-        pathlib.Path(TEMP_DIR).mkdir(exist_ok=True)
-        with open("template.html", "r", encoding="utf-8") as f:
+        pathlib.Path(output_dir).mkdir(exist_ok=True)
+        pathlib.Path(temp_dir).mkdir(exist_ok=True)
+        with open(pathlib.Path(__file__).parent / "template.html", "r", encoding="utf-8") as f:
             self.template = Template(f.read())
 
         self.distance = distance
@@ -65,35 +69,57 @@ class ThermalReportGenerator:
         self.ambient = ambient
         self.reflection = reflection
         self.brightness = brightness
-        self.palette = palette
+        self.palette = palette if isinstance(palette, ThermalPalette) else ThermalPalette._member_map_.get(palette, ThermalPalette.iron_red)
+        self.colorbar_width = colorbar_width
+        self.border = "border: 1px solid #000;" if colorbar_border else ""
+        self.input_dir = input_dir
+        self.output_dir = output_dir
+        self.cli_path = cli_path
+        self.temp_dir = temp_dir
 
         self.semaphore = asyncio.Semaphore(max_workers)
         self.executor = ProcessPoolExecutor(max_workers=max_workers)
 
-    def get_metadata(self, img_path):
+    def get_metadata(self, img_path: str | pathlib.Path):
         """从 APP1 Marker 提取元数据"""
-        with open(img_path, 'rb') as f:
-            tags = exifread.process_file(f, builtin_types=True)
+        with pyexiv2.Image(str(img_path)) as img:
+            tags = img.read_xmp()
+            tags.update(img.read_exif())
+
+        if not tags.get("Xmp.drone-dji.ImageSource", "") == "InfraredCamera":
+            return None
             
-        def get_tag(key):
-            return str(tags.get(key, "N/A"))
+        def get_tag(key: str):
+            result = str(tags.get(key, "N/A"))
+            if result != "N/A" and key.startswith("Exif"):
+                cache = []
+                for i in result.split(' '):
+                    if i.find('/'):
+                        j = i.split('/')
+                        if j[0].isdecimal() and j[1].isdecimal():
+                            cache.append(float(j[0]) / float(j[1]))
+                    else:
+                        cache.append(i)
+                if isinstance(cache[0], float):
+                    result = cache if len(cache) > 1 else str(cache[0])
+            return result
 
         return {
-            "model": get_tag('Image Model'),
-            "sn": get_tag('EXIF BodySerialNumber'),
-            "focal_length": get_tag('EXIF FocalLength'),
-            "aperture": f"{float(get_tag('EXIF FNumber')):.1f}" if get_tag('EXIF FNumber') != "N/A" else "N/A",
-            "create_time": get_tag('EXIF DateTimeOriginal'),
-            "gps": f"{convert_to_decimal(get_tag('GPS GPSLatitude')):.6f}, {convert_to_decimal(get_tag('GPS GPSLongitude')):.6f}"
+            "model": get_tag('Xmp.tiff.Model'),
+            "sn": get_tag('Xmp.drone-dji.DroneSerialNumber'),
+            "focal_length": get_tag('Exif.Photo.FocalLength'),
+            "aperture": f"{float(get_tag('Exif.Photo.FNumber')):.1f}" if get_tag('Exif.Photo.FNumber') != "N/A" else "N/A",
+            "create_time": datetime.datetime.fromisoformat(get_tag('Xmp.xmp.CreateDate')).strftime("%Y/%m/%d %H:%M:%S"),
+            "gps": f"{convert_to_decimal(get_tag('Exif.GPSInfo.GPSLatitude')):.6f}, {convert_to_decimal(get_tag('Exif.GPSInfo.GPSLongitude')):.6f}"
         }
 
-    async def process_thermal_async(self, img_path: str, task_id: str):
+    async def process_thermal_async(self, img_path: str | pathlib.Path, task_id: str):
         """调用 DJI SDK CLI 处理图像"""
-        raw_out = os.path.join(TEMP_DIR, f"{task_id}.raw")
+        raw_out = pathlib.Path(self.temp_dir) / f"{task_id}.raw"
         
         # 生成伪彩色图像数据 (RGB 格式)
         cmd = [
-            "-a", "process", "-s", img_path, "-o", raw_out, 
+            "-a", "process", "-s", str(img_path), "-o", raw_out, 
             "-p", self.palette.name,
             "--distance", str(self.distance),
             "--humidity", str(self.humidity),
@@ -103,7 +129,7 @@ class ThermalReportGenerator:
             "--brightness", str(self.brightness)
         ]
         proc = await asyncio.create_subprocess_exec(
-            CLI_PATH, *cmd,
+            self.cli_path, *cmd,
             stdout=asyncio.subprocess.PIPE,
             stderr=asyncio.subprocess.PIPE
         )
@@ -116,7 +142,7 @@ class ThermalReportGenerator:
         for line in result.split('\n'):
             if "adaptive range" in line:
                 temps = line.split('[')[1].split(']')[0].split(',')
-                min_temp, max_temp = f"{float(temps[0].strip()):.2f}", f"{float(temps[1].strip()):.2f}"
+                min_temp, max_temp = f"{float(temps[0].strip()):.1f}", f"{float(temps[1].strip()):.1f}"
             if "image  width" in line:
                 w = int(line.split(':')[-1].strip())
             if "image height" in line:
@@ -126,10 +152,10 @@ class ThermalReportGenerator:
         with open(raw_out, "rb") as f:
             img = Image.frombytes("RGB", (w, h), f.read())
         
-        final_img_path = os.path.join(TEMP_DIR, f"{task_id}.png")
+        final_img_path = pathlib.Path(self.temp_dir) / f"{task_id}.png"
         img.save(final_img_path)
 
-        if os.path.exists(raw_out): os.remove(raw_out)
+        raw_out.unlink(missing_ok=True)
         
         return final_img_path, min_temp, max_temp, w, h
 
@@ -148,8 +174,11 @@ class ThermalReportGenerator:
         """单个文件的完整处理流水线"""
         async with self.semaphore:
             task_id = uuid.uuid4().hex
-            full_path = os.path.join(INPUT_DIR, img_name)
-            pdf_path = os.path.join(TEMP_DIR, f"{task_id}.pdf")
+            if not pathlib.Path(img_name).is_absolute():
+                full_path = pathlib.Path(self.input_dir) / img_name
+            else:
+                full_path = pathlib.Path(img_name)
+            pdf_path = pathlib.Path(self.temp_dir) / f"{task_id}.pdf"
             
             try:
                 # SDK 处理
@@ -157,6 +186,8 @@ class ThermalReportGenerator:
                 
                 # 元数据提取 (同步)
                 meta = self.get_metadata(full_path)
+                if meta is None:
+                    return None, None, img_name, "No InfraredCamera Image / Cannot find DJI XMP"
                 
                 # 渲染 HTML
                 html_out = self.template.render(
@@ -170,18 +201,28 @@ class ThermalReportGenerator:
                     emissivity=f"{self.emissivity}", 
                     reflection=f"{self.reflection}",
                     ambient=f"{self.ambient}",
+                    colorbar_width = self.colorbar_width,
+                    colorbar_border = self.border,
                     **meta
                 )
                 
                 # 进程池渲染 PDF
                 await self.render_pdf_worker(html_out, pdf_path)
-                return pdf_path, png_path, img_name
-            except Exception:
+                return pdf_path, png_path, img_name, None
+            except Exception as e:
                 traceback.print_exc()
-                return None, None, img_name
-            
-    async def run(self):
-        images = [f for f in os.listdir(INPUT_DIR) if f.lower().endswith(('.jpg', '.jpeg'))]
+                return None, None, img_name, e
+    
+    async def run(self, image_abs_paths: Optional[list[str | pathlib.Path]] = None) -> AsyncGenerator[tuple[int, bool], None]:
+        if not image_abs_paths:
+            images = [f for f in os.listdir(str(self.input_dir)) if f.lower().endswith(('.jpg', '.jpeg'))]
+        else:
+            images = []
+            for f in image_abs_paths:
+                if not pathlib.Path(f).is_absolute():
+                    raise ValueError("Path in file list must be absolute!")
+                if str(f).lower().endswith(('.jpg', '.jpeg')):
+                    images.append(str(f))
         if not images:
             print("未发现待处理图片")
             return
@@ -197,8 +238,10 @@ class ThermalReportGenerator:
             if result[0] is not None and result[1] is not None:
                 results[result[2]] = (result[0], result[1])
                 print(f"完成: {result[2]}")
+                yield len(images), True
             else:
-                print(f"失败: {result[2]}")
+                print(f"失败: {result[2]} ({result[3]})")
+                yield len(images), False
         
         # 筛选有效的 PDF 路径
         pdf_paths = [r[0] for r in results.values()]
@@ -211,14 +254,14 @@ class ThermalReportGenerator:
                 with fitz.open(p) as f:
                     merged_pdf.insert_pdf(f)
             
-            output_file = os.path.join(OUTPUT_DIR, f"DJI_Thermal_Report_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.pdf")
+            output_file = pathlib.Path(self.output_dir) / f"DJI_Thermal_Report_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.pdf"
             merged_pdf.save(output_file)
             merged_pdf.close()
             print(f"\n报告已生成: {output_file}")
 
             # 清理所有临时文件
             for f in pdf_paths + all_temp_imgs:
-                if os.path.exists(f): os.remove(f)
+                pathlib.Path(f).unlink(missing_ok=True)
         
         self.executor.shutdown()
 
@@ -226,5 +269,14 @@ if __name__ == "__main__":
     if os.name == 'nt':
         asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
     
-    gen = ThermalReportGenerator(palette=ThermalPalette.iron_red)
-    asyncio.run(gen.run())
+    gen = ThermalReportGenerator(
+        input_dir="./input_images",
+        output_dir="./reports",
+        temp_dir="./temp_dir",
+        cli_path=r"D:\Test\dji_thermal_sdk_v1.8_20250829\utility\bin\windows\release_x64\dji_irp.exe",
+        palette=ThermalPalette.iron_red
+    )
+    async def _internal():
+        async for i in gen.run():
+            pass
+    asyncio.run(_internal())
